b3c3fdcaf43e6c0c2147582729dfbc219f63a776
David Mc Ken <david.mcken@gmail.com>
2025-01-28 04:33:17 -0400

    Add actual call to parsing mib to cli.

--- src/mib2zabbix/cli.py
+++ src/mib2zabbix/cli.py
@@ -1,11 +1,17 @@
 '''mib2zabbix CLI module'''
 
 
+# System imports
+import logging
 
+# Local imports
+from . import utils
 
-
+logger = logging.getLogger(__name__)
 
 def main():
     """Main CLI function
     """
     print("Entered mib2zabbix:main")
+
+    utils.parse_mib('/home/dmcken/code/mib2zabbix/test/')
